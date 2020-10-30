29a591ffa682f14b4fc191e3f2ad80231daa11e7
Matt Werth <matthewwerth@gmail.com>
2020-10-29 22:09:18 -0400

    Added Constructor For TreeClass

--- src/com/treeoxygenoutput/Main.java
+++ src/com/treeoxygenoutput/Main.java
@@ -3,8 +3,7 @@ package com.treeoxygenoutput;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
+        TreeClass appleTree = new TreeClass(1,2,"Fruit",true);
+        System.out.println(appleTree.getTree());
     }
 }
--- src/com/treeoxygenoutput/TreeClass.java
+++ src/com/treeoxygenoutput/TreeClass.java
@@ -1,15 +1,23 @@
 package com.treeoxygenoutput;
 
-import com.sun.source.tree.Tree;
-
 public class TreeClass {
-    private Integer treeAge; //Perhaps year planted would be better? Who would use this? Orchards?
+    private static Integer treeID;
+    private static Integer treeAge; //Perhaps year planted would be better? Who would use this? Orchards?
     private String treeType;
     private Boolean treeInGoodHealth;
 
-    public TreeClass(Integer treeAge, String treeType, Boolean treeInGoodHealth) {
+    public TreeClass(Integer treeID, Integer treeAge, String treeType, Boolean treeInGoodHealth) {
+        this.treeID = treeID;
         this.treeAge = treeAge;
         this.treeType = treeType;
         this.treeInGoodHealth = treeInGoodHealth;
     }
+
+    public String getTree() {
+        return String.valueOf("Tree ID: " + TreeClass.treeID + " Tree Age: " + TreeClass.treeAge);
+    }
+
 }
+
+//create new trees and pass in data.
+//build a database of trees and their growth rate
