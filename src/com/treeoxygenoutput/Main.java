9d81552251c2cc08db296e1b74068b2ade6f6cdf
Matt Werth <matthewwerth@gmail.com>
2020-10-30 08:57:55 -0400

    Added Additional Tree Methods

--- src/com/treeoxygenoutput/Main.java
+++ src/com/treeoxygenoutput/Main.java
@@ -1,9 +1,22 @@
 package com.treeoxygenoutput;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
-        TreeClass appleTree = new TreeClass(1,2,"Fruit",true);
-        System.out.println(appleTree.getTreeID());
+    public static void main(String[] args) throws IOException {
+
+//        UUID id = new UUID();
+//        id = (Integer) id;
+
+        // We can use this method stand alone - because it is STATIC ! ! !
+        String orchard = TreeClass.plantTrees(56372, 5, 4);
+        System.out.println(orchard);
+
+        ArrayList orchardID = TreeClass.setID(6);
+
+        System.out.println(orchardID.toString());
     }
 }
--- src/com/treeoxygenoutput/TreeClass.java
+++ src/com/treeoxygenoutput/TreeClass.java
@@ -1,5 +1,9 @@
 package com.treeoxygenoutput;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class TreeClass {
     private Integer treeID;
     private Integer treeAge; //Perhaps year planted would be better? Who would use this? Orchards?
@@ -17,7 +21,40 @@ public class TreeClass {
         return treeID;
     }
 
+    public static ArrayList setID(Integer numberOfTrees) throws IOException {
+        if (numberOfTrees <= 0) throw new IOException("IOException Occurred");
+
+        ArrayList<UUID> listOfTreeID = new ArrayList<>();
+
+        for(int i = 0; i < numberOfTrees; i++) {
+            UUID uniqueKey = UUID.randomUUID();
+            listOfTreeID.add(uniqueKey);
+        }
+        return listOfTreeID;
+    }
+
+    //METHOD THAT TAKES OUR ARRAYLIST OF IDS AND CREATES A TREE OBJECT FOR EACH ONE.
+
+    // Is there any way to not loop within a loop but assign each tree a unique id?
+    public static String plantTrees(Integer gpsCoordinates, Integer rows, Integer columns) throws IOException {
+        if (rows <= 0 || columns <= 0) throw new IOException("IOException Occurred");
+
+        String visualTreeMap = "";
+        int treeRows = 1;
+
+        for(int i = 0; i < rows; i++) { // I need to pass in a unique ID for each tree - any way without nesting loops?
+            visualTreeMap += "|" + String.format("  Tree Row %d |", treeRows).repeat(columns) + "\n";
+            treeRows+=1;
+        }
+        return ("Tree Location " + gpsCoordinates + "\n") + visualTreeMap;
+    }
+
+
 }
 
-//create new trees and pass in data.
-//build a database of trees and their growth rate
+//create method to calculate tree oxygen output at each year of life - assuming healthy tree at average growth.
+//use the method we created to make math tables and have that create tree tables?
+//visually show how much oxygen a tree produces at each year of its life.
+//visually show how many people each tree provides oxygen for.
+
+//do the same for fruit output. How many people can the tree/group of trees feed.
